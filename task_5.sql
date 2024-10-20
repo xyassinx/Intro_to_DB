8318a7c1b0226eb338c61f4df7bf791bcda73314
xyassinx <83932207+xyassinx@users.noreply.github.com>
2024-10-20 23:03:01 +0300

    Add files via upload

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Insert a single row into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
